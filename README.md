a6836c0f8f4d801abd000936786372896e5165d7
Ajaz Rana <RanaAjaz@hotmail.com>
2022-11-06 01:17:28 -0400

    first commit"

---
+++ README.md
@@ -0,0 +1,6 @@
+# GPT-3-Finetune
+
+- Source: https://github.com/PradipNichite
+- Reference: https://www.youtube.com/c/PradipNichiteAI
+- install dependencies: `pip install -r requirements.txt`
+- Run application `streamlit run app.py`
---
+++ app.py
@@ -0,0 +1,31 @@
+
+import streamlit as st
+import os
+import openai
+openai.api_key = ""
+
+api_key = st.sidebar.text_input("APIkey", type="password")
+os.environ["OPENAI_API_KEY"] = api_key
+openai.api_key = api_key
+
+st.header("GPT-3 Intent Classifier")
+question  = st.text_area("Enter Text/Question")
+button = st.button("Generate Reply")
+
+def generate_reply(review):
+    response = openai.Completion.create(
+    model="davinci:ft-personal-2022-10-30-06-01-45",
+    prompt=f"This is an intent classification bot. Identify the intent of the give question.\n\nQuestion:{question}\n\nIntent:",
+    temperature=0,
+    max_tokens=5,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    st.write(response)
+    return response.choices[0].text
+
+if button and question:
+    with st.spinner("Generating Reply..."):
+        reply = generate_reply(question)
+    st.write(reply)
